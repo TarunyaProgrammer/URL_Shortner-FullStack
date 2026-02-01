117cda0a7687997f5a82e0f9cf8f4c9b955918aa
Tarunya K <tarunya.programmer@gmail.com>
2026-02-01 21:23:56 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
 import express from "express";
 const app = express();
+import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+dotenv.config("./.env");
+import fielcabinet from "./src/config/mongo.config.js";
 
-app.listen(5000, () => {
-  console.log("Server is running!! on ", `localhost:5000`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/create", (req, res) => {
+  const url = req.body.url;
+  console.log(url);
+  res.send(nanoid(7));
+});
+
+app.listen(3000, () => {
+  fielcabinet();
+  console.log("Server is running!! on ", `http://localhost:3000/`);
 });
